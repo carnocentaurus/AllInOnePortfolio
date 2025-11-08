a3e1c102e03b8f15d8cda5c91a6868515b6365b5
EJay <carnocentaurus@gmail.com>
2025-11-08 12:09:56 +0800

    included name and email input fields

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ const aboutPage = document.getElementById("aboutPage");
 const projectsPage = document.getElementById("projectsPage");
 const contactPage = document.getElementById("contactPage");
 
+const ageDisplay = document.getElementById("ageDisplay");
+
+const form = document.getElementById("form");
+const submitBtn = form.querySelector("button[type='submit']");
+
 const pages = [aboutPage, projectsPage, contactPage];
 
 function openPage(index) {
@@ -14,7 +19,6 @@ function closePage(index) {
   pages[index].style.display = "none";
   homePage.style.display = "flex";
 }
-
 
 // dynamic age display
 const birthYear = 2004;
@@ -31,19 +35,17 @@ if (currentMonth < birthMonth || (currentMonth === birthMonth && currentDate < b
     age --;
 }
 
-document.getElementById("ageDisplay").textContent = age;
+ageDisplay.textContent = age;
 
-const form = document.getElementById('form');
-const submitBtn = form.querySelector('button[type="submit"]');
 
-form.addEventListener('submit', async (e) => {
+// form handler
+form.addEventListener("submit", async (e) => {
     e.preventDefault();
 
     const formData = new FormData(form);
     formData.append("access_key", "6d011ba0-63b9-4bce-95a5-892b48feac36");
 
     const originalText = submitBtn.textContent;
-
     submitBtn.textContent = "Sending...";
     submitBtn.disabled = true;
 
@@ -56,16 +58,15 @@ form.addEventListener('submit', async (e) => {
         const data = await response.json();
 
         if (response.ok) {
-            alert("Success! Your message has been sent.");
+            alert("Your message has been sent!");
             form.reset();
         } 
         else {
             alert("Error: " + data.message);
         }
-
     } 
     catch (error) {
-        alert("Something went wrong. Please try again.");
+        alert("Something went wrong. Please try again");
     } 
     finally {
         submitBtn.textContent = originalText;

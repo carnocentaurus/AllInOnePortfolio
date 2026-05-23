ee0d6047f6886826ff1f019e48ba4116aad5924e
EJay <carnocentaurus@gmail.com>
2026-05-23 14:09:14 +0800

    GatekeeperPortal: connected to supabase

---
+++ GatekeeperPortal/GatekeeperPortal.js
@@ -0,0 +1,3 @@
+const SUPABASE_PROJECT_URL = 'https://yigdkkczudxmkdchzzwv.supabase.co';
+const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_adM37VUFSG7zLzlMcLOEAw_mBDnKYII';
+const supabaseClient = supabase.createClient(SUPABASE_PROJECT_URL, SUPABASE_PUBLISHABLE_KEY);

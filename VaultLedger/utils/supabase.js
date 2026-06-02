084cb432b7acb6ded622a3cd19acea2cbff48085
EJay <carnocentaurus@gmail.com>
2026-06-02 13:06:37 +0800

    VaultLedger #9: button ids and window object

--- VaultLedger/utils/supabase.js
+++ VaultLedger/utils/supabase.js
@@ -1,4 +1,4 @@
 const SUPABASE_PROJECT_URL = 'https://ofpdzthgvopnqmfyirrm.supabase.co';
 const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_v8NAtRJHNmKxEPCxwaI5Mg_oiDGvvtI';
 
-export const supabaseClient = supabase.createClient(SUPABASE_PROJECT_URL, SUPABASE_PUBLISHABLE_KEY);
+export const supabaseClient = window.supabase.createClient(SUPABASE_PROJECT_URL, SUPABASE_PUBLISHABLE_KEY);

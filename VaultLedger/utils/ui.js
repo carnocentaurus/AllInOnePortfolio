6816e65264c100521dc7525eb678d0f98e75c8f9
EJay <carnocentaurus@gmail.com>
2026-06-03 13:50:40 +0800

    VaultLedger #11: centralized dom elements

--- VaultLedger/utils/ui.js
+++ VaultLedger/utils/ui.js
@@ -1,55 +1,57 @@
 // ui.js
 
-// home
-const homeDiv = document.getElementById('home-div');
-const startBtn = document.getElementById('start-btn');
+export const domElements = {
+  // home
+  homeDiv: document.getElementById('home-div'),
+  startBtn: document.getElementById('start-btn'),
 
-// auth
-const authDiv = document.getElementById('auth-div');
-const authForm = document.getElementById('auth-form');
-const emailInput = document.getElementById('email-input');
-const passwordInput = document.getElementById('password-input');
-const authBtn = document.getElementById('auth-btn');
-const authAccountText = document.getElementById('auth-account-text');
-const authInsteadText = document.getElementById('auth-instead-text');
-const authBackBtn = document.getElementById('auth-back-btn');
+  // auth
+  authDiv: document.getElementById('auth-div'),
+  authForm: document.getElementById('auth-form'),
+  emailInput: document.getElementById('email-input'),
+  passwordInput: document.getElementById('password-input'),
+  authBtn: document.getElementById('auth-btn'),
+  authAccountText: document.getElementById('auth-account-text'),
+  authInsteadText: document.getElementById('auth-instead-text'),
+  authBackBtn: document.getElementById('auth-back-btn'),
 
-// dashboard
-const dashboardDiv = document.getElementById('dashboard-div');
-const currentBalanceDisplay = document.getElementById('current-balance-display');
-const withdrawBtn = document.getElementById('withdraw-btn');
-const manageAccountBtn = document.getElementById('manage-account-div');
-const logOutBtn = document.getElementById('log-out-btn');
-const dashboardBackBtn = document.getElementById('dashboard-back-btn');
+  // dashboard
+  dashboardDiv: document.getElementById('dashboard-div'),
+  currentBalanceDisplay: document.getElementById('current-balance-display'),
+  withdrawBtn: document.getElementById('withdraw-btn'),
+  manageAccountBtn: document.getElementById('manage-account-div'),
+  logOutBtn: document.getElementById('log-out-btn'),
+  dashboardBackBtn: document.getElementById('dashboard-back-btn'),
 
-// withdraw
-const withdrawDiv = document.getElementById('withdraw-div');
-const withdrawInput = document.getElementById('withdraw-input');
-const withdrawConfirmBtn = document.getElementById('withdraw-confirm-div');
-const withdrawCancelBtn = document.getElementById('withdraw-cancel-btn');
+  // withdraw
+  withdrawDiv: document.getElementById('withdraw-div'),
+  withdrawInput: document.getElementById('withdraw-input'),
+  withdrawConfirmBtn: document.getElementById('withdraw-confirm-div'),
+  withdrawCancelBtn: document.getElementById('withdraw-cancel-btn'),
 
-// deposit
-const depositDiv = document.getElementById('deposit-div');
-const depositInput = document.getElementById('deposit-input');
-const depositConfirmBtn = document.getElementById('deposit-confirm-btn');
-const depositCancelBtn = document.getElementById('deposit-cancel-btn');
+  // deposit
+  depositDiv: document.getElementById('deposit-div'),
+  depositInput: document.getElementById('deposit-input'),
+  depositConfirmBtn: document.getElementById('deposit-confirm-btn'),
+  depositCancelBtn: document.getElementById('deposit-cancel-btn'),
 
-// manage account
-const manageAccountDiv = document.getElementById('manage-accout-div');
-const changeEmailBtn = document.getElementById('change-email-btn');
-const changePasswordBtn = document.getElementById('change-password-btn');
-const manageAccountBackBtn = document.getElementById('manage-account-back-btn');
+  // manage account
+  manageAccountDiv: document.getElementById('manage-accout-div'),
+  changeEmailBtn: document.getElementById('change-email-btn'),
+  changePasswordBtn: document.getElementById('change-password-btn'),
+  manageAccountBackBtn: document.getElementById('manage-account-back-btn'),
 
-// change email
-const changeEmailDiv = document.getElementById('change-email-div');
-const changeEmailForm = document.getElementById('change-email-form');
-const newEmailInput = document.getElementById('new-email-input');
-const changeEmailConfirmBtn = document.getElementById('change-email-confirm-btn');
-const changeEmailCancelBtn = document.getElementById('change-email-cancel-btn');
+  // change email
+  changeEmailDiv: document.getElementById('change-email-div'),
+  changeEmailForm: document.getElementById('change-email-form'),
+  newEmailInput: document.getElementById('new-email-input'),
+  changeEmailConfirmBtn: document.getElementById('change-email-confirm-btn'),
+  changeEmailCancelBtn: document.getElementById('change-email-cancel-btn'),
 
-// change password
-const changePasswordDiv = document.getElementById('change-password-div');
-const changePasswordForm = document.getElementById('change-password-form');
-const newPasswordInput = document.getElementById('new-password-input');
-const changePasswordConfirmBtn = document.getElementById('change-password-confirm-btn');
-const changePasswordCancelBtn = document.getElementById('change-password-cancel-btn');
+  // change password
+  changePasswordDiv: document.getElementById('change-password-div'),
+  changePasswordForm: document.getElementById('change-password-form'),
+  newPasswordInput: document.getElementById('new-password-input'),
+  changePasswordConfirmBtn: document.getElementById('change-password-confirm-btn'),
+  changePasswordCancelBtn: document.getElementById('change-password-cancel-btn')
+};

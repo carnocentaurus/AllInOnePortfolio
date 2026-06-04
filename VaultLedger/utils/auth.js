fab36f6e61fa83cec89939ab3934e147ff506cf5
EJay <carnocentaurus@gmail.com>
2026-06-04 14:56:28 +0800

    VaultLedger #17: log in and and log out functions

--- VaultLedger/utils/auth.js
+++ VaultLedger/utils/auth.js
@@ -4,12 +4,26 @@ import {supabaseClient} from './supabase.js';
 
 export async function signUpUser(email, password) {
     if (!String(email).includes('@gmail.com')) {
-        alert('Email must contain `@gmail.com` at the end!');
+        throw new Error('Email must contain `@gmail.com` at the end!');
         return;
     }
 
     const {data, error} = await supabaseClient.auth.signUp({email, password});
-    if (error) alert(`Sign up error: ${error.message}`);
+    if (error) throw error;
 
     return data;
+}
+
+
+export async function logInUser(email, password) {
+    const {data, error} = await supabaseClient.auth.signInWithPassword({email, password});
+    if (error) throw error;
+
+    return data;
+}
+
+
+export async function logOutUser() {
+    const {error} = await supabaseClient.auth.signOut();
+    if (error) throw error;
 }
